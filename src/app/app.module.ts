7a04113a80c1809e787a430174962bd0f83596c3
Yunus Emre Aksu <emre_aksu@live.com>
2023-02-15 11:34:53 +0300

    Appbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppbarComponent } from './components/appbar/appbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
